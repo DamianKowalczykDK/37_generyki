561e10c7ec614c3f753df442f2228d6609c31ea2
Damian <damiankowalczyk3688@gmail.com>
2025-03-19 22:43:13 +0100

    process_pairs add

--- app2.py
+++ app2.py
@@ -3,9 +3,13 @@ from typing import Callable
 def filter_elements[T](elements: list[T], condition_fn: Callable[[T], bool]) -> list[T]:
     return [element for element in elements if condition_fn(element)]
 
+def process_pairs[K, V](data: dict[K, V], modifier_fn: Callable[[V], V]) -> dict[K, V]:
+    return {key: modifier_fn(V) for key, V in data.items()}
+
 
 def main() -> None:
     print(filter_elements([1, 2, 3, 4], lambda x: x > 1))
+    print(process_pairs({'a': 1, 'b': 2, 'c': 3}, lambda x: x * 2))
 
 if __name__ == '__main__':
     main()
