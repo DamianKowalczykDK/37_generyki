855c770931a50c894ccb3c835b385029519bdb5e
Damian <damiankowalczyk3688@gmail.com>
2025-03-19 22:46:08 +0100

    merge items add

--- app2.py
+++ app2.py
@@ -6,10 +6,14 @@ def filter_elements[T](elements: list[T], condition_fn: Callable[[T], bool]) -> 
 def process_pairs[K, V](data: dict[K, V], modifier_fn: Callable[[V], V]) -> dict[K, V]:
     return {key: modifier_fn(V) for key, V in data.items()}
 
+def merge_items[T1, T2, T3](items1: T1, items2: T2, items3: T3) -> tuple[T1, T2, T3]:
+    return items1, items2, items3
+
 
 def main() -> None:
     print(filter_elements([1, 2, 3, 4], lambda x: x > 1))
     print(process_pairs({'a': 1, 'b': 2, 'c': 3}, lambda x: x * 2))
+    print(merge_items(1, 'a', [22, 33]))
 
 if __name__ == '__main__':
     main()
